ad23d3e25a67e0f0caec84a829ea3a7de208c879
lilliali-umsi <55165046+lilliali-umsi@users.noreply.github.com>
2019-09-10 18:35:29 -0400

    Create si506_problem_set_01.py

---
+++ si506_problem_set_01.py
@@ -0,0 +1,12 @@
+# PROBLEM 01F (25 points)
+# Substitute your U-M email address for all occurrences of the word "Dutch" using
+# the appropriate built-in function in the zen_of_python string. Assign the modified
+# Zen of Python string to a new variable named "zen_of_python_uniqname".
+
+# BEGIN 01F SOLUTION
+zen_of_python_uniqname = ''
+
+print(''.join(['zen_of_python_uniqname=', zen_of_python_uniqname, '\n']))
+# END 01F SOLUTION
+
+# END PROBLEM SET
